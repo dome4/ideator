0a909a86cdafc6a2a7a9224b54c3d870277a608c
dome4 <dominic.seitz@gmx.de>
2019-06-14 10:36:24 +0200

    idea service and model created

---
+++ src/app/shared/models/idea.model.ts
@@ -0,0 +1,12 @@
+export interface Idea {
+
+  id: number;
+  title: string;
+  businessIdea: string;
+  usp: string;
+  customers: string;
+  businessModel: string;
+  competitors: string;
+  team: string;
+  marketBarriers: string;
+}
---
+++ src/app/shared/services/idea.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { Idea } from '../models/idea.model';
+
+@Injectable()
+export class IdeaService {
+
+  // api url
+  private API_URL = environment.ideaApiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getPersons(): Observable<Idea[]> {
+    return this.http.get<Idea[]>('assets/ideas.json');
+  }
+}
