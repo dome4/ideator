e643142d21ee23c7a12f12422111c3bb1d43ef4e
dome4 <dominic.seitz@gmx.de>
2019-06-14 10:24:49 +0200

    basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IdeaListComponent } from './idea-list/idea-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/idea-list',
+    pathMatch: 'full'
+  },
+  { path: 'idea-list', component: IdeaListComponent },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
