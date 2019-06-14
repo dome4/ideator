6777d3f6ad91fa3d9fd0cef6d5789ee4841af0bf
dome4 <dominic.seitz@gmx.de>
2019-06-14 10:20:11 +0200

    idea-list component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IdeaListComponent } from './idea-list/idea-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IdeaListComponent
   ],
   imports: [
     BrowserModule,
