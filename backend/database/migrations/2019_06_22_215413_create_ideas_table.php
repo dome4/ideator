10a872f7390178a1b3d1effd0b75835523750b6a
dome4 <dominic.seitz@gmx.de>
2019-06-22 23:59:18 +0200

    migrations created

---
+++ backend/database/migrations/2019_06_22_215413_create_ideas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIdeasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ideas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('businessIdea');
+            $table->string('usp');
+            $table->string('customers');
+            $table->string('businessModel');
+            $table->string('competitors');
+            $table->string('team');
+            $table->string('marketBarriers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ideas');
+    }
+}
