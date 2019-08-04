49fa2351c107b53e9839a7a37b106e9357d3e6c8
dome4 <dominic.seitz@gmx.de>
2019-08-04 19:51:30 +0200

    webpack for lektor installed

---
+++ landing-page/README.md
@@ -0,0 +1,42 @@
+# Landing Page build with lektor
+
+One Paragraph of project description goes here
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+```
+Give examples
+```
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+End with an example of getting some data out of the system or using it for a little demo
+
+## Running the tests
+
+Explain how to run the automated tests for this system
+
+### Run Server
+
+`lektor server -f webpack`
+
+## Deployment
+
+`lektor build -f webpack`
+
+## Built With
+
+- [Lektor](https://github.com/lektor/lektor)
+- [Webpack for Lektor](https://github.com/lektor/lektor-webpack-support)
+
+## Authors
+
+- **Dominic Seitz** - _Initial work_ - [dome4](https://github.com/dome4)
---
+++ landing-page/webpack/webpack.config.js
@@ -0,0 +1,67 @@
+const isDevelopment = process.env.NODE_ENV === "development";
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var path = require("path");
+
+module.exports = {
+  entry: {
+    app: "./js/main.js",
+    styles: "../sass/styles.scss"
+  },
+  output: {
+    path: path.dirname(__dirname) + "/assets/static/gen",
+    filename: "[name].js"
+  },
+
+  module: {
+    rules: [
+      ///...
+      {
+        test: /\.module\.s(a|c)ss$/,
+        loader: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              localIdentName: "[name]__[local]___[hash:base64:5]",
+              camelCase: true,
+              sourceMap: isDevelopment
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      }
+      ///...
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".scss"]
+  },
+  ///...
+  plugins: [
+    ///...
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? "[name].css" : "[name].[hash].css",
+      chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css"
+    })
+  ]
+};
